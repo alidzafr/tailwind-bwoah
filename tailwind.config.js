7e822527c0216a6ec277dceb137e66b02a8efd88
alidzafr <alidzafathur@gmail.com>
2025-10-23 16:41:19 +0700

    custom font and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["*.html"],
   theme: {
     extend: {
+      colors: {
+        'ijo-royo': '#15803d'
+      }
+    },
+    fontFamily: {
+      'roboto': 'Roboto',
+      'bitcount': 'Bitcount Grid Single'
+    },
+    extend: {
       spacing: {
         'custom-spacing': '10px'
       },
@@ -11,6 +20,9 @@ module.exports = {
       },
       margin: {
         'custom-margin': '200px'
+      },
+      width: {
+        'custom-width': '50rem'
       }
     },
   },
